cd2d82bd82f3ca0b0ccc1781b68465ad6bf96a20
jager <jager@c8a1e018-962f-4fc7-b34a-25f67a7ad721>
2012-07-26 18:50:21 +0000

    jade template rendszer integralasa
    git-svn-id: svn+ssh://vertigoweb.hu/repo/trunk/www/mesh-owners-site@1427 c8a1e018-962f-4fc7-b34a-25f67a7ad721

--- http/middlewares/getHandlers.js
+++ http/middlewares/getHandlers.js
@@ -2,10 +2,10 @@
  * @author Grigore András Zsolt
  */
 
-function rootReqHandler(req, res) {
-    res.render
+function root(req, res) {
+	res.render('index')
 }
-exports.rootReqHandler = rootReqHandler;
+exports.root = root;
 
 function login(req, res) {
 	res.render('login');
@@ -18,22 +18,28 @@ function tester(req, res) {
 exports.tester = tester;
 
 function internalServerError(req, res) {
-	res.render('500.jade', { locals: {
-                  title : 'The Server Encountered an Error'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX'
-                 ,error: err
-                },status: 500 });
+	res.render('500.jade', {
+		locals: {
+			title : 'The Server Encountered an Error'
+			,description: ''
+			,author: ''
+			,analyticssiteid: 'XXXXXXX'
+			,error: err
+		},
+		status: 500 
+	});
 }
 exports.internalServerError = internalServerError;
 
 function notFound(req, res) {
-	res.render('404.jade', { locals: {
-                  title : '404 - Not Found'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX'
-                },status: 404 });
+	res.render('404.jade', {
+		locals: {
+			title : '404 - Not Found'
+			,description: ''
+			,author: ''
+			,analyticssiteid: 'XXXXXXX'
+		},
+		status: 404 
+	});
 }
 exports.notFound = notFound;
