c63edbdf2d82f6aba775880d7a0e8db03fed42c6
grigo <grigo@c8a1e018-962f-4fc7-b34a-25f67a7ad721>
2012-08-07 20:32:50 +0000

    [AGV web control] html-ek kigyakása + tennivaló lista
    git-svn-id: svn+ssh://vertigoweb.hu/repo/trunk/www/mesh-owners-site@1432 c8a1e018-962f-4fc7-b34a-25f67a7ad721

--- core.js
+++ core.js
@@ -11,11 +11,24 @@ var	MosWebSockets = require('./websockets/MosWebSockets');
 /* Parse config to memory */
 global.configuration = config.createGlobalConfig(fs.readFileSync('mos.config.json', 'utf-8'));
 
-var mosHttp = new MosHttp(global.configuration.http);
-var mosWebSockets = new MosWebSockets(global.configuration.websockets)
-mosHttp.listen();
-mosWebSockets.listen(mosHttp);
+if (global.configuration.http.enabled) {
+	var mosHttp = new MosHttp(global.configuration.http);
+	mosHttp.listen();
+}
 
-//configolható fícsör (enable/disable)
-//kigyakni minden felesleget
-//kitörölni a mosSiteokat SVN-ből
+if (global.configuration.websockets) {
+	var mosWebSockets = new MosWebSockets(global.configuration.websockets)
+	mosWebSockets.listen(mosHttp);
+}
+//Tennivalók:
+//
+//Express 3-ra migrálás
+// - http modul refaktor sessionstore!
+// - templétek véglegesítése (login, tester, error)
+// - i18n alapok cimkékhez (en/hu pl.: 404 error - Requested content not found! -  Nem található a kért tartalom!)
+// - socketio hozzáigazítás
+//configolható fícsör (pl.: websockets.enabled = false akkor nem indul a modul.)
+// - unit testek (pl.: ha egy adott middleware assertFailure akkor WARN middleware nem indult mert assertFail)
+//session store interface és Redis tesztek
+//kigyakni minden felesleget:
+// - (tester app) HTML refaktor + JS refaktor; statikből ami nem kell kidobni!
